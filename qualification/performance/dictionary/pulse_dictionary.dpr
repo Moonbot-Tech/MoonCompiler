1c0ed0371e50532733097d787c6dc2dfbef9b194
Mike <mike@moonbot.pro>
2026-08-24 00:27:31 +0300

    test(pulse): split large dictionary lookup costs

--- qualification/performance/dictionary/pulse_dictionary.dpr
+++ qualification/performance/dictionary/pulse_dictionary.dpr
@@ -37,18 +37,28 @@ var
   StringValues: TStringArray;
   PreparedUInt64Short: TUInt64Dictionary;
   PreparedUInt64Long: TUInt64Dictionary;
+  PreparedUInt64LongHalfLoad: TUInt64Dictionary;
   PreparedStringKeyShort: TStringKeyDictionary;
   PreparedStringKeyLong: TStringKeyDictionary;
   PreparedStringValueShort: TStringValueDictionary;
   PreparedStringValueLong: TStringValueDictionary;
+  PreparedStringValueLongHalfLoad: TStringValueDictionary;
 
-procedure FillUInt64Dictionary(Dictionary: TUInt64Dictionary; Count: Integer);
+procedure FillUInt64Dictionary(Dictionary: TUInt64Dictionary; Count: Integer;
+  HalfLoad: Boolean = False);
 var
   I: Integer;
 begin
+  {$ifdef FPC}
+  if HalfLoad then
+    Dictionary.MaxLoadFactor := 0.5;
+  {$endif}
   Dictionary.Capacity := Count;
   for I := 0 to Count - 1 do
     Dictionary.Add(UInt64Keys[I], UInt64Keys[I] xor UInt64($D1B54A32D192ED03));
+  if HalfLoad and (Dictionary.Capacity <> 32768) then
+    raise Exception.CreateFmt('unexpected half-load capacity: %d',
+      [Dictionary.Capacity]);
 end;
 
 procedure FillStringKeyDictionary(Dictionary: TStringKeyDictionary;
@@ -62,13 +72,20 @@ begin
 end;
 
 procedure FillStringValueDictionary(Dictionary: TStringValueDictionary;
-  Count: Integer);
+  Count: Integer; HalfLoad: Boolean = False);
 var
   I: Integer;
 begin
+  {$ifdef FPC}
+  if HalfLoad then
+    Dictionary.MaxLoadFactor := 0.5;
+  {$endif}
   Dictionary.Capacity := Count;
   for I := 0 to Count - 1 do
     Dictionary.Add(UInt64Keys[I], StringValues[I]);
+  if HalfLoad and (Dictionary.Capacity <> 32768) then
+    raise Exception.CreateFmt('unexpected half-load capacity: %d',
+      [Dictionary.Capacity]);
 end;
 
 procedure InitializeData;
@@ -91,24 +108,30 @@ begin
 
   PreparedUInt64Short := TUInt64Dictionary.Create;
   PreparedUInt64Long := TUInt64Dictionary.Create;
+  PreparedUInt64LongHalfLoad := TUInt64Dictionary.Create;
   PreparedStringKeyShort := TStringKeyDictionary.Create;
   PreparedStringKeyLong := TStringKeyDictionary.Create;
   PreparedStringValueShort := TStringValueDictionary.Create;
   PreparedStringValueLong := TStringValueDictionary.Create;
+  PreparedStringValueLongHalfLoad := TStringValueDictionary.Create;
   FillUInt64Dictionary(PreparedUInt64Short, ShortCount);
   FillUInt64Dictionary(PreparedUInt64Long, LongCount);
+  FillUInt64Dictionary(PreparedUInt64LongHalfLoad, LongCount, True);
   FillStringKeyDictionary(PreparedStringKeyShort, ShortCount);
   FillStringKeyDictionary(PreparedStringKeyLong, LongCount);
   FillStringValueDictionary(PreparedStringValueShort, ShortCount);
   FillStringValueDictionary(PreparedStringValueLong, LongCount);
+  FillStringValueDictionary(PreparedStringValueLongHalfLoad, LongCount, True);
 end;
 
 procedure FinalizeData;
 begin
+  PreparedStringValueLongHalfLoad.Free;
   PreparedStringValueLong.Free;
   PreparedStringValueShort.Free;
   PreparedStringKeyLong.Free;
   PreparedStringKeyShort.Free;
+  PreparedUInt64LongHalfLoad.Free;
   PreparedUInt64Long.Free;
   PreparedUInt64Short.Free;
   Finalize(StringValues);
@@ -153,6 +176,34 @@ begin
       if Dictionary.TryGetValue(UInt64Misses[I], Value) then
         Inc(Result);
     end;
+  Result := Result xor UInt64(Dictionary.Count);
+end;
+
+function LookupUInt64Hits(Iterations, Count: Integer;
+  Dictionary: TUInt64Dictionary): UInt64;
+var
+  I, J: Integer;
+  Value: UInt64;
+begin
+  Result := 0;
+  for J := 1 to Iterations do
+    for I := 0 to Count - 1 do
+      if Dictionary.TryGetValue(UInt64Keys[I], Value) then
+        Result := Result xor Value;
+  Result := Result xor UInt64(Dictionary.Count);
+end;
+
+function LookupUInt64Misses(Iterations, Count: Integer;
+  Dictionary: TUInt64Dictionary): UInt64;
+var
+  I, J: Integer;
+  Value: UInt64;
+begin
+  Result := 0;
+  for J := 1 to Iterations do
+    for I := 0 to Count - 1 do
+      if Dictionary.TryGetValue(UInt64Misses[I], Value) then
+        Inc(Result);
   Result := Result xor UInt64(Dictionary.Count);
 end;
 
@@ -277,6 +328,34 @@ begin
   Result := Result xor UInt64(Dictionary.Count);
 end;
 
+function LookupStringValueHits(Iterations, Count: Integer;
+  Dictionary: TStringValueDictionary): UInt64;
+var
+  I, J: Integer;
+  Value: UnicodeString;
+begin
+  Result := 0;
+  for J := 1 to Iterations do
+    for I := 0 to Count - 1 do
+      if Dictionary.TryGetValue(UInt64Keys[I], Value) then
+        Result := Result xor UInt64(Length(Value));
+  Result := Result xor UInt64(Dictionary.Count);
+end;
+
+function LookupStringValueMisses(Iterations, Count: Integer;
+  Dictionary: TStringValueDictionary): UInt64;
+var
+  I, J: Integer;
+  Value: UnicodeString;
+begin
+  Result := 0;
+  for J := 1 to Iterations do
+    for I := 0 to Count - 1 do
+      if Dictionary.TryGetValue(UInt64Misses[I], Value) then
+        Inc(Result);
+  Result := Result xor UInt64(Dictionary.Count);
+end;
+
 function ChurnStringValue(Iterations, Count: Integer;
   Dictionary: TStringValueDictionary): UInt64;
 var
@@ -311,6 +390,14 @@ function CaseUInt64Lookup100(Iterations: Integer): UInt64;
 begin Result := LookupUInt64(Iterations, ShortCount, PreparedUInt64Short); end;
 function CaseUInt64Lookup10000(Iterations: Integer): UInt64;
 begin Result := LookupUInt64(Iterations, LongCount, PreparedUInt64Long); end;
+function CaseUInt64LookupHits10000(Iterations: Integer): UInt64;
+begin Result := LookupUInt64Hits(Iterations, LongCount, PreparedUInt64Long); end;
+function CaseUInt64LookupMisses10000(Iterations: Integer): UInt64;
+begin Result := LookupUInt64Misses(Iterations, LongCount, PreparedUInt64Long); end;
+function CaseUInt64LookupHalfLoad10000(Iterations: Integer): UInt64;
+begin
+  Result := LookupUInt64(Iterations, LongCount, PreparedUInt64LongHalfLoad);
+end;
 function CaseUInt64Churn100(Iterations: Integer): UInt64;
 begin Result := ChurnUInt64(Iterations, ShortCount, PreparedUInt64Short); end;
 function CaseUInt64Churn10000(Iterations: Integer): UInt64;
@@ -345,6 +432,21 @@ function CaseStringValueLookup100(Iterations: Integer): UInt64;
 begin Result := LookupStringValue(Iterations, ShortCount, PreparedStringValueShort); end;
 function CaseStringValueLookup10000(Iterations: Integer): UInt64;
 begin Result := LookupStringValue(Iterations, LongCount, PreparedStringValueLong); end;
+function CaseStringValueLookupHits10000(Iterations: Integer): UInt64;
+begin
+  Result := LookupStringValueHits(Iterations, LongCount,
+    PreparedStringValueLong);
+end;
+function CaseStringValueLookupMisses10000(Iterations: Integer): UInt64;
+begin
+  Result := LookupStringValueMisses(Iterations, LongCount,
+    PreparedStringValueLong);
+end;
+function CaseStringValueLookupHalfLoad10000(Iterations: Integer): UInt64;
+begin
+  Result := LookupStringValue(Iterations, LongCount,
+    PreparedStringValueLongHalfLoad);
+end;
 function CaseStringValueChurn100(Iterations: Integer): UInt64;
 begin Result := ChurnStringValue(Iterations, ShortCount, PreparedStringValueShort); end;
 function CaseStringValueChurn10000(Iterations: Integer): UInt64;
@@ -371,6 +473,16 @@ begin
   PulseRunCase('pulse_dictionary', 'u64-u64-lookup-mixed-10000', 'rtl',
     'TDictionary<UInt64,UInt64> hit/miss lookup, 10000 items',
     @CaseUInt64Lookup10000, LongCount * 2, Profile, SelectedCase, Found);
+  PulseRunCase('pulse_dictionary', 'u64-u64-lookup-hit-10000', 'rtl',
+    'TDictionary<UInt64,UInt64> hit-only lookup, 10000 items',
+    @CaseUInt64LookupHits10000, LongCount, Profile, SelectedCase, Found);
+  PulseRunCase('pulse_dictionary', 'u64-u64-lookup-miss-10000', 'rtl',
+    'TDictionary<UInt64,UInt64> miss-only lookup, 10000 items',
+    @CaseUInt64LookupMisses10000, LongCount, Profile, SelectedCase, Found);
+  PulseRunCase('pulse_dictionary', 'u64-u64-lookup-halfload-10000', 'rtl',
+    'TDictionary<UInt64,UInt64> hit/miss lookup at 10000/32768 load',
+    @CaseUInt64LookupHalfLoad10000, LongCount * 2, Profile, SelectedCase,
+    Found);
   PulseRunCase('pulse_dictionary', 'u64-u64-churn-100', 'rtl',
     'TDictionary<UInt64,UInt64> remove/reinsert half, 100 items',
     @CaseUInt64Churn100, ShortCount, Profile, SelectedCase, Found);
@@ -421,6 +533,17 @@ begin
   PulseRunCase('pulse_dictionary', 'u64-string-lookup-mixed-10000', 'rtl',
     'TDictionary<UInt64,UnicodeString> hit/miss lookup, 10000 items',
     @CaseStringValueLookup10000, LongCount * 2, Profile, SelectedCase, Found);
+  PulseRunCase('pulse_dictionary', 'u64-string-lookup-hit-10000', 'rtl',
+    'TDictionary<UInt64,UnicodeString> hit-only lookup, 10000 items',
+    @CaseStringValueLookupHits10000, LongCount, Profile, SelectedCase, Found);
+  PulseRunCase('pulse_dictionary', 'u64-string-lookup-miss-10000', 'rtl',
+    'TDictionary<UInt64,UnicodeString> miss-only lookup, 10000 items',
+    @CaseStringValueLookupMisses10000, LongCount, Profile, SelectedCase,
+    Found);
+  PulseRunCase('pulse_dictionary', 'u64-string-lookup-halfload-10000', 'rtl',
+    'TDictionary<UInt64,UnicodeString> hit/miss lookup at 10000/32768 load',
+    @CaseStringValueLookupHalfLoad10000, LongCount * 2, Profile,
+    SelectedCase, Found);
   PulseRunCase('pulse_dictionary', 'u64-string-churn-100', 'rtl+mm',
     'TDictionary<UInt64,UnicodeString> remove/reinsert half, 100 items',
     @CaseStringValueChurn100, ShortCount, Profile, SelectedCase, Found);
